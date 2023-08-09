6d8d91bf63270a75efe0bcffd19203f1d9295bce
Akhil Kamsala <akhilvadla46@gmail.com>
2023-08-09 10:18:20 +0530

    added git repo

--- app/Chart/page.js
+++ app/Chart/page.js
@@ -3,32 +3,31 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as echarts from 'echarts';
 import { Card, SpinLoading } from 'antd-mobile';
+import moment from 'moment/moment';
 
 export default function ChartDisplay({ crypto = "ethereum" }) {
     const ref = useRef(null);
     const [data, setData] = useState({ x: [], y: [] });
     const [loading,setLoading]=useState(true);
 
-    useEffect(() => {
-
-        const dom = document.getElementById("chart");
+    useEffect(() => {        
         var myChart = echarts.init(ref.current);
         var option;
 
         option = {
             xAxis: {
                 type: 'category',
-                data: [...data?.x]
+                data: [...data?.x],                
             },
             yAxis: {
-                type: 'value',
+                type: 'value',                
             },
             series: [
                 {
                     data: [...data?.y],
                     type: 'line',
                     smooth: true,
-                    areaStyle: { color: 'skyblue' }
+                    areaStyle: { color: 'lightgreen' }
                 }
             ]
         };
@@ -44,8 +43,8 @@ export default function ChartDisplay({ crypto = "ethereum" }) {
             const data = await fetch(`https://api.coingecko.com/api/v3/coins/${crypto}/market_chart?vs_currency=usd&days=365&interval=daily`, { method: "GET" });
             setLoading(false);
             const json = await data.json();
-            const X = json.prices.map((el) => el[0]);
-            const Y = json.prices.map((el) => el[1]);
+            const X = json.prices.map((el) => moment.unix(el[0]).format('MMMM'));
+            const Y = json.prices.map((el) => (el[1]));
             setData({ x: X, y: Y })
         }
         fetchData(crypto)
@@ -54,7 +53,7 @@ export default function ChartDisplay({ crypto = "ethereum" }) {
 
     return (
         <Card className='p-2 border border-blue-200 m-2'>
-            <h1 className='text-xl text-center text-blue-500'>{crypto.toLocaleUpperCase()}</h1>
+            <h1 className='text-xl text-center text-blue-500'>{String(crypto).toUpperCase()}</h1>
             {loading && <div className='text-center'><SpinLoading/></div>}
             <div ref={ref} id='chart' style={{ minWidth: '32vw', minHeight: '32vh' }} className='text-center'/>                
             
--- app/page.js
+++ app/page.js
@@ -1,45 +1,50 @@
 "use client"
-
-
 import ChartDisplay from './Chart/page'
 import { useEffect, useState } from 'react'
 import { Switch } from 'antd-mobile'
 import { Select } from 'antd'
+import { useLocalStorage } from './hooks/useLocalStorage'
+
 
 export default function Home() {
   const coins = ['ethereum', 'dogecoin', 'solana'];
   const options = coins.map((ele => ({ label: ele, value: ele })));
-  
-  const [selected, setSelected] = useState(JSON.parse(localStorage?.getItem("selected"))||[]);
+  const [selected, setSelected] = useLocalStorage('selected',[]);  
   const [type, setType] = useState("row");
 
+  
 
-  /* useEffect(()=>{
-      const selectedItems = localStorage.getItem("selected");
-      setSelected(JSON.parse(selectedItems));      
-  },[]); */
-
-  useEffect(() => {
-    localStorage.setItem("selected", JSON.stringify(selected));
-
+  useEffect(() => {    
+    localStorage.setItem("selected", JSON.stringify(selected));            
   }, [selected]);
-
 
 
   return (
     <main className="flex flex-col items-center justify-evenly	p-24">
       <h1 className='text-3xl font-bold mb-4'>Welcome to Crypto Tracker</h1>
-      <Select
+
+      {selected.length==0 && <Select
+        mode="multiple"
+        allowClear
+        style={{
+          width: '100%',
+        }}        
+        placeholder="Please select"
+        onChange={(e) => { console.log("Changed",e);setSelected(e) }}
+        options={options}
+      />}
+      {selected.length>0 && <Select
         mode="multiple"
         allowClear
         style={{
           width: '100%',
         }}
-        defaultValue={[...selected]}
+        defaultValue={selected}
         placeholder="Please select"
-        onChange={(e) => { setSelected(e) }}
+        onChange={(e) => { console.log("Changed",e);setSelected(e) }}
         options={options}
-      />
+      />}
+      
       <div className='mb-2 mt-2'>
         <label className='mx-2'>Row</label>
         <Switch onChange={(value) => {
