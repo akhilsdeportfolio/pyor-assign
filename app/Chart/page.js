52ca8822bb2b09bb02a4550456358cc0cd917439
Akhil Kamsala <akhilvadla46@gmail.com>
2023-08-09 21:53:31 +0530

    added configurable pages ooption

--- app/Chart/page.js
+++ app/Chart/page.js
@@ -4,14 +4,24 @@ import React, { useEffect, useRef, useState } from 'react';
 import * as echarts from 'echarts';
 import { Card, SpinLoading } from 'antd-mobile';
 import moment from 'moment/moment';
+import { Select } from 'antd';
 
-export default function ChartDisplay({ crypto = "ethereum" }) {
+export default function ChartDisplay({ crypto="ethereum",direction}) {
     const ref = useRef(null);
+
+
     const [data, setData] = useState({ x: [], y: [] });
     const [loading,setLoading]=useState(true);
+    const coins = ['ethereum', 'dogecoin', 'solana'];
+    const options = coins.map((ele => ({ label: ele, value: ele })));
+    const [isSelected,setIsSelected]=useState(true);
+    const [selected,setSelected]=useState([crypto]);
+
+
+    
 
     useEffect(() => {        
-        var myChart = echarts.init(ref.current);
+        var myChart = echarts.init(ref.current||document.getElementById("chart"));
         var option;
 
         option = {
@@ -34,7 +44,22 @@ export default function ChartDisplay({ crypto = "ethereum" }) {
 
         myChart.setOption(option);
 
+
+        return ()=>{
+            myChart.dispose()
+        }
+
     }, [ref, data]);
+
+
+    // ANT DESIGN 
+  // MUI DESIGN 
+
+    
+
+
+
+
 
 
     useEffect(() => {
@@ -43,6 +68,7 @@ export default function ChartDisplay({ crypto = "ethereum" }) {
             const data = await fetch(`https://api.coingecko.com/api/v3/coins/${crypto}/market_chart?vs_currency=usd&days=365&interval=daily`,{});
             setLoading(false);
             const json = await data.json();
+            console.log("JSON",json);
             const X = json.prices.map((el) => moment.unix(el[0]).format('MMMM'));
             const Y = json.prices.map((el) => (el[1]));
             setData({ x: X, y: Y })
@@ -51,12 +77,46 @@ export default function ChartDisplay({ crypto = "ethereum" }) {
     }, []);
 
 
+    useEffect(() => {
+
+        async function fetchData(crypto) {
+            const data = await fetch(`https://api.coingecko.com/api/v3/coins/${crypto}/market_chart?vs_currency=usd&days=365&interval=daily`,{});
+            setLoading(false);
+            const json = await data.json();
+            console.log("JSON",json);
+            const X = json.prices.map((el) => moment.unix(el[0]).format('MMMM'));
+            const Y = json.prices.map((el) => (el[1]));
+            setData({ x: X, y: Y })
+        }
+        fetchData(selected)
+    }, [selected]);
+
+    
+
+    if(!crypto)
+    return null;
+
+
     return (
+
+        <div className='shadow-sm' style={{display:'flex',flexDirection:direction}}>
+
+        <Select         
+        onChange={(selected)=>{
+            console.log(selected);
+            setSelected(selected);
+        }}
+        defaultValue={selected}
+        options={options}
+        />
+        
+
         <Card className='p-2 border border-blue-200 m-2'>
-            <h1 className='text-xl text-center text-blue-500'>{String(crypto).toUpperCase()}</h1>
+            <h1 className='text-xl text-center text-blue-500'>{String(selected).toUpperCase()}</h1>
             {loading && <div className='text-center'><SpinLoading/></div>}
             <div ref={ref} id='chart' style={{ minWidth: '32vw', minHeight: '32vh' }} className='text-center'/>                
             
         </Card>
+        </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,18 +1,26 @@
 "use client"
 import ChartDisplay from './Chart/page'
 import { useEffect, useState } from 'react'
-import { Switch } from 'antd-mobile'
-import { Select } from 'antd'
+import { Button, Switch } from 'antd-mobile'
+import { Input, Radio, Select } from 'antd'
 import { useLocalStorage } from './hooks/useLocalStorage'
+import {Modal} from 'antd';
 
 
 export default function Home() {
   const coins = ['ethereum', 'dogecoin', 'solana'];
   const options = coins.map((ele => ({ label: ele, value: ele })));
   const [selected, setSelected] = useLocalStorage('selected',[]);  
-  const [type, setType] = useState("row");
+  const [type, setType] = useState("col");
+
+  const [config,setConfig]=useState({});
+  
 
   
+
+  useEffect(()=>{
+    console.log(config)
+  },[config]);
 
   useEffect(() => {    
     localStorage.setItem("selected", JSON.stringify(selected));            
@@ -23,7 +31,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-evenly	p-24">
       <h1 className='text-3xl font-bold mb-4'>Welcome to Crypto Tracker</h1>
 
-      {selected.length==0 && <Select
+    {/*   {selected.length==0 && <Select
         mode="multiple"
         allowClear
         style={{
@@ -43,19 +51,50 @@ export default function Home() {
         placeholder="Please select"
         onChange={(e) => { setSelected(e) }}
         options={options}
-      />}
+      />} */}
+            
+    
+            <div style={{minWidth:'200px'}}>
+      <label>Select Coin</label>  
+      <Select        
+        allowClear
+        style={{
+          width: '100%',
+        }}
+        defaultValue={'ethereum'}
+        placeholder="Please select"
+        onChange={(e) => {setConfig({...config,coin:e}) }}
+        options={options}
+      />
+      <label>
+        Select Direction
+      </label>
+      <div>
+        <Radio.Group onChange={(val)=>setConfig({...config,direction:val.target.value})} defaultValue={'row'}>
+        <Radio name='direction' value={"row"}>Row</Radio>
+        <Radio name='direction' value={"column"}>Column</Radio>
+        </Radio.Group>
+      </div>
+      <Button  onClick={()=>{
+          setSelected([...selected,config])
+          setConfig({});
+      }}>
+        Add Page to Dashboard
+      </Button>
+      </div>
+
+      <br/>
+
+      <div className={`flex flex-row justify-evenly px-2 flex-wrap`}>
+        {selected?.map((el,index) => (<ChartDisplay key={index} crypto={el.coin} direction={el.direction}/>))}
+      </div>
       
-      <div className='mb-2 mt-2'>
-        <label className='mx-2'>Row</label>
-        <Switch onChange={(value) => {
-          if (value) setType("col")
-          else setType("row")
-        }} defaultChecked={type === 'row' ? false : true} />
-        <label className='mx-2'>Column</label>
-      </div>
-      <div className={`flex ${type === 'row' ? 'flex-row' : 'flex-col'} justify-evenly px-2 flex-wrap`}>
-        {selected?.map(coin => (<ChartDisplay key={coin} crypto={coin} />))}
-      </div>
+
+      
+
+      
+
+
     </main>
   )
 }
